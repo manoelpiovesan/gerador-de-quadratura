dcd0d5b08ce4be52403f33897606bf9571933c56
Victor Luis Teixeira Reis <victorluis2002@gmail.com>
2022-11-29 23:42:36 -0300

    implementando diamond difference

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+// document.querySelector('#btn-calcular').addEventListener('click', ()=>{
+//     Quadratura();
+// })
+
 document.querySelector('#btn-calcular').addEventListener('click', ()=>{
-    Quadratura();
+    diamondDifference();
 })
+
+
 
 function Quadratura(){
     document.querySelector('#table-data-body').innerHTML = ''
@@ -24,9 +30,6 @@ function Quadratura(){
 
     var wPos = []
     var wNeg = []
-
-    console.log(m)
-
 
     for(var i = 0; i < m-1; i++){
 
@@ -71,7 +74,7 @@ function Quadratura(){
     
     generateTable(mi, w);
 
-    return mi, w;
+    return [mi, w];
 }
 
 function generateTable(mi, w){
@@ -80,3 +83,72 @@ function generateTable(mi, w){
     });
     
 }
+
+
+function diamondDifference() {
+    let [mi, w] = Quadratura()
+    let sigmaT = 1  // Seção de choque macroscópica total
+    let sigmaSo = 0.5   // Seção de choque de espalhamento macroscópica total 
+    let Q = 0   // Fonte
+    let N = 4   // Quantidade de direções do fluxo
+    let CC = [1,1,0,0]
+    let h = 20 // tamanho da malha (comprimento)
+    let nodos = 200 // quantidade de divisões (j)
+    let deltaH = h/nodos // Intervalo de cada j
+
+    let mesh = []
+
+    // Cria um array de linhas e j colunas
+    for(let i = 0; i < N; i++){
+        mesh.push(Array(nodos-1).fill(0)) // criando com um elemento a menos para completar depois com a condição de contorno
+    }
+
+
+    // adiciona as condições de contorno (primeira metade no início e segunda metade no final
+    CC.forEach((element, index)=>{
+        if(index < (CC.length/2)){
+            mesh[index].unshift(element)
+        }else{
+            mesh[index].push(element)
+        }
+    })
+    
+    // varredura da esquerda pra direita para todos os J's (Mi > 0)
+    for(var j = 1; j < nodos; j++){
+
+        var sumSSJ = 0
+        // somatorio do SSJ começando do inicio
+        for(var n = 1; n <= N; n++){
+           sumSSJ += w[n-1] * ((mesh[n-1][j-1] + mesh[n-1][j])/2)
+        }
+
+        var SSJ = (sigmaSo/2) * sumSSJ
+
+        for(var m = 0; m < N/2; m++){
+            mesh[m][j] = (mesh[m][j-1] * ((mi[m]/deltaH)-(sigmaT/2))+(SSJ)+(Q))/((mi[m]/deltaH)+(sigmaT/2))
+            console.log(`PSI(${m+1},${j}): ${mesh[m][j]}`)
+        }
+    }
+    
+    // varredura da direita pra esquerda para todos os J's (Mi < 0)
+    for(var j = nodos-2; j >= 0; j--){
+
+        var sumSSJ = 0
+        // somatorio do SSJ começando do final
+        for(var n = 1; n <= N; n++){
+           sumSSJ += w[n-1] * ((mesh[n-1][j] + mesh[n-1][j+1])/2)
+        }
+
+        var SSJ = (sigmaSo/2) * sumSSJ
+        
+        for(var m = N/2; m < N; m++){
+
+            mesh[m][j] = (mesh[m][j+1] * ((mi[m]/deltaH)+(sigmaT/2))-(SSJ)-(Q))/((mi[m]/deltaH)-(sigmaT/2))
+            
+            console.log(`PSI(${m+1},${j}): ${mesh[m][j]}`)
+        }
+    }
+    
+    console.log(mesh)
+}
+
